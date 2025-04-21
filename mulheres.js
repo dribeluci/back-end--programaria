4a235d59f1a6c7a041d138741a7f8e065477ebcd
dribeluci <adrianabeluci@hotmail.com>
2025-04-21 18:57:00 -0300

    insere a nova versão do projeto integrado com o mongodb

--- mulheres.js
+++ mulheres.js
@@ -1,37 +1,148 @@
- const express= require ("express")
- //*pegue o pacote express e deixe disponivel no meu código para usar sempre quando for necessário. 
+const express= require ("express") //pegue o pacote express e deixe disponivel no meu código para usar sempre quando for necessário. 
 const router= express.Router()
- const app = express()
-const porta= 3333                                                                                                               
-//* "Crie uma constante com o apelido de porta e guarde a informação 3333"
+const cors = require('cors') // estou trazendo o pacote cors que permite iconsumir essa api no front-end
+const conectaBancoDeDados = require('./bancoDeDados')// aqui estou ligando ao arquivo bancoDeDados
+conectaBancoDeDados() // estou chamando a função que conecta o banco de dados
 
-const mulheres = [
-    {
-        nome: 'Simara Conceição',
-        imagem: 'https://bit.ly/3LJIyOF',
-        minibio: 'Desenvolvedora e instrutora',                                                                      
-    },
-    {
-        nome: 'Iana Chan',  
-        imagem: 'https://bit.ly/3JCXBqP',
-        minibio: 'CEO & Founder da PrograMaria',
-    },
-    {
-        nome: 'Luana Pimentel',
-        imagem: 'https://bit.ly/3FKpFaz',
-        minibio: 'Senior Staff Software Engineer',
-    }
-]
-   
-function mostraMulheres(request, response){
-    response.json(mulheres)
+
+const Mulher = require('./mulherModel')
+
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+const porta= 3333                                                                                                               
+
+
+//GET
+async function mostraMulheres(request, response){
+      try{
+        const mulheresVindasDoBancoDeDados = await Mulher.find()
+        response.json(mulheresVindasDoBancoDeDados)
+      }catch (erro){
+        console.log(erro)
+
+      }
 }
+
+//POST
+async function criaMulher(request, response) {
+
+  const novaMulher = new Mulher({
+
+      nome: request.body.nome,
+
+      imagem: request.body.imagem,
+
+      minibio: request.body.minibio,
+
+      citacao: request.body.citacao
+
+  })
+
+
+
+  try {
+
+      const mulherCriada = await novaMulher.save()
+
+      response.status(201).json(mulherCriada)
+
+  } catch (erro) {
+
+      console.log(erro)
+
+  }
+
+}
+
+
+
+//PATCH
+
+async function corrigeMulher(request, response) {
+
+  try {
+
+      const mulherEncontrada = await Mulher.findById(request.params.id)
+
+
+
+      if (request.body.nome) {
+
+          mulherEncontrada.nome = request.body.nome
+
+      }
+
+ 
+
+      if (request.body.minibio) {
+
+          mulherEncontrada.minibio = request.body.minibio
+
+      }
+
+ 
+
+      if (request.body.imagem) {
+
+          mulherEncontrada = request.body.imagem
+
+      }
+
+
+
+      if (request.body.citacao) {
+
+          mulherEncontrada = request.body.citacao
+
+      }
+
+
+
+      const mulherAtualizadaNoBancoDeDados = await mulherEncontrada.save()
+
+
+
+      response.json(mulherAtualizadaNoBancoDeDados)
+
+  } catch (erro) {
+
+      console.log(erro)
+
+  }
+
+}
+
+
+//DELETE
+async function deletaMulher(request, response) {
+
+  try {
+
+      await Mulher.findByIdAndDelete(request.params.id)
+
+      response.json({ messagem: 'Mulher deletada com sucesso!'})
+
+  } catch(erro) {
+
+      console.log(erro)
+
+  }
+
+}
+
 function mostraPorta() {
+
     console.log("Servidor criado e rodando na porta ", porta)  
 }
-//* "Crie uma função com o apelido de mostraPorta e quando ela for executada eu quero que você mostre no terminal o texto "Servidor
-// criado e rodando na porta e por fim mostre a porta que eu guardei na constante "
 
-app.use(router.get('/mulheres', mostraMulheres))
+app.use(router.delete('/mulheres/:id', deletaMulher))
+app.use(router.patch('/mulheres/:id', corrigeMulher))
+
+app.use(router.post('/mulheres', criaMulher))
+app.use(router.get('/mulheres', mostraMulheres)) // configurei rota Post /mulheres
 app.listen(porta, mostraPorta)
-//*Após ouvir a porta do númemro 3333 eu quero que você execute a função mostraPorta.
+//Após ouvir a porta do númemro 3333 eu quero que você execute a função mostraPorta.
+
